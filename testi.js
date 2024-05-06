1757e2be34661fbab21e9732eaea5f6af6ecaf3a
Valheri <159899638+Valheri@users.noreply.github.com>
2024-05-06 10:04:53 +0300

    päivitys
    päivitystä

--- testi.js
+++ testi.js
@@ -1,86 +1,113 @@
 let calendarData = [
       {
-            "date": "01-04-2024",
+            "date": "2024-01-04",
             "opened": false,
             "image_closed": "doorclosed.jpg",
             "image_opened": "door.jpg",
             "link": "http://example.com/1",
             "desc": "jotain",
-            "map": ""
+            "map": `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d129708.19179114768!2d24.57343458998777!3d59.47158519202321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46929499df5616bf%3A0x400b36d18fc6270!2sTallinn%2C%20Estonia!5e0!3m2!1sen!2sfi!4v1714974541499!5m2!1sen!2sfi" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
       },
       {
-            "date": "01-04-2024",
+            "date": "2024-05-01",
             "opened": false,
-            "image_closed": "",
-            "image_opened": "",
+            "image_closed": "doorclosed.jpg",
+            "image_opened": "door.jpg",
             "link": "",
-            "desc": "",
-            "map": ""
+            "desc": "kaks",
+            "map": ``
       },
       {
-            "date": "01-04-2024",
+            "date": "2024-06-03",
             "opened": false,
-            "image_closed": "",
-            "image_opened": "",
+            "image_closed": "doorclosed.jpg",
+            "image_opened": "door.jpg",
             "link": "",
-            "desc": "",
-            "map": ""
+            "desc": "3",
+            "map": ``
       },
       {
-            "date": "01-04-2024",
+            "date": "2024-06-10",
             "opened": false,
-            "image_closed": "",
+            "image_closed": "doorclosed.jpg",
             "image_opened": "",
             "link": "",
-            "desc": "",
-            "map": ""
+            "desc": "4",
+            "map": ``
       }, {
-            "date": "01-04-2024",
+            "date": "2024-06-17",
             "opened": false,
-            "image_closed": "",
+            "image_closed": "doorclosed.jpg",
             "image_opened": "",
             "link": "",
-            "desc": "",
-            "map": ""
+            "desc": "5",
+            "map": ``
       }, {
-            "date": "01-04-2024",
+            "date": "2024-06-24",
             "opened": false,
-            "image_closed": "",
+            "image_closed": "doorclosed.jpg",
             "image_opened": "",
             "link": "",
-            "desc": "",
-            "map": ""
+            "desc": "6",
+            "map": ``
       }, {
-            "date": "01-04-2024",
+            "date": "2024-07-01",
             "opened": false,
-            "image_closed": "",
+            "image_closed": "doorclosed.jpg",
             "image_opened": "",
             "link": "",
-            "desc": "",
-            "map": ""
+            "desc": "7",
+            "map": ``
       }, {
-            "date": "01-04-2024",
+            "date": "2024-07-08",
             "opened": false,
-            "image_closed": "",
+            "image_closed": "doorclosed.jpg",
             "image_opened": "",
             "link": "",
-            "desc": "",
-            "map": ""
+            "desc": "8",
+            "map": ``
       }, {
-            "date": "01-04-2024",
+            "date": "2024-07-15",
             "opened": false,
-            "image_closed": "",
+            "image_closed": "doorclosed.jpg",
             "image_opened": "",
             "link": "",
-            "desc": "",
-            "map": ""
+            "desc": "9",
+            "map": ``
       }, {
-            "date": "01-04-2024",
+            "date": "2024-07-22",
             "opened": false,
-            "image_closed": "",
+            "image_closed": "doorclosed.jpg",
             "image_opened": "",
             "link": "",
-            "desc": "",
-            "map": ""
+            "desc": "10",
+            "map": ``
+      }, {
+            "date": "2024-07-29",
+            "opened": false,
+            "image_closed": "doorclosed.jpg",
+            "image_opened": "",
+            "link": "",
+            "desc": "11",
+            "map": ``
+      }, {
+            "date": "2024-08-05",
+            "opened": false,
+            "image_closed": "doorclosed.jpg",
+            "image_opened": "",
+            "link": "",
+            "desc": "12",
+            "map": ``
+      }, {
+            "date": "2024-08-12",
+            "opened": false,
+            "image_closed": "doorclosed.jpg",
+            "image_opened": "",
+            "link": "",
+            "desc": "13",
+            "map": ``
       }
+      
+      
+      
 ]
