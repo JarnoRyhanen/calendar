a3ded1c56211036cb2ca606b1a9d19f0a912b319
JarnoRyhanen <jarno.ryhanen02@gmail.com>
2024-05-06 12:19:34 +0300

    Linkki toimiva
    Klikkaamalla kuvaa modaalissa pääsee nyt tapahtuman nettisivulle.

--- modal.js
+++ modal.js
@@ -5,30 +5,23 @@ var modal = document.getElementById("myModal");
 
 function openModalPopup(calendarData) {
 
-    
     let description = calendarData.desc;
     let image = calendarData.img;
     let linkToEvent = calendarData.link;
     let location = calendarData.map;
-    console.log(description); 
-    console.log(location); 
-    console.log(linkToEvent); 
-
-
+  
     
     let modalImg = document.getElementById("modalImage");
     let captionText = document.getElementById("caption");
     let locationMap = document.getElementById("location");
-    let link = document.getElementById("link");
+    let link = document.getElementById("imageLink");
 
-     modal.style.display = "block";
-     
+     modal.style.display = "block";     
      modalImg.src = image;
+     link.href = linkToEvent; 
+     
      captionText.innerHTML = description;
      locationMap.innerHTML = location;
-
-
-
 }
 
 // Get the <span> element that closes the modal
