ebe170ca87e5f901b6f41dc595074b271a774fe9
Valheri <159899638+Valheri@users.noreply.github.com>
2024-05-06 14:59:00 +0300

    highllight korjaus
    highlight nimeämiset selvemmäksi

--- calendar.js
+++ calendar.js
@@ -6,17 +6,18 @@ calendarData.forEach((data, index) => {
     const holder = document.createElement('div');
     const door = document.createElement('div');
     door.classList.add('door');
+
     const para = document.createElement('p');
     para.classList.add('teksti');
   
     if (data.date <= todayString) {
-        door.classList.add('highlight');
+        door.classList.add('greenhighlight');
         door.innerHTML = `<img src="./Kuvat/door.jpg" width="100%" alt="Door">`;
         door.addEventListener('click', () => {
             openModalPopup(data);
         });
     } else {
-       door.classList.add('highlight2');
+       door.classList.add('redhighlight');
        door.innerHTML = `<img src="./Kuvat/doorclosed.jpg" width="100%" alt="Door">`;
     }
   
