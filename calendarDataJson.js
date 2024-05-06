3d8e5b50266bf17fcfc9202f699d41a99d5a1076
JarnoRyhanen <jarno.ryhanen02@gmail.com>
2024-05-06 14:29:11 +0300

    Poistettu jsonista opened kentät

--- calendarDataJson.js
+++ calendarDataJson.js
@@ -1,7 +1,6 @@
 let calendarData = [
       {
             "date": "2024-01-04",
-            "opened": false,
             "img": "./Kuvat/maailmakylassa.jpg",
             "link": "https://maailmakylassa.fi",
             "desc": "Maailma kylässä -festivaali 25.–26.5.2024 la 11–21 su 11–19",
@@ -10,7 +9,6 @@ let calendarData = [
       },
       {
             "date": "2024-05-01",
-            "opened": false,
             "img": "./Kuvat/linnanmaki.jpg",
             "link": "https://www.linnanmaki.fi/fi/hupitekemista/tapahtumat/rolle-huvipuistossa",
             "desc": "Rolle-pelle huvipuistossa 30.04.–25.08. Linnanmäen Huvipuisto",
@@ -18,7 +16,6 @@ let calendarData = [
       },
       {
             "date": "2023-06-03",
-            "opened": false,
             "img": "./Kuvat/sidewaysHelsinki.jpg",
             "link": "https://www.sidewayshelsinki.fi",
             "desc": "Sideways Festival, 13.–15.6.2024 Helsingin jäähalliin ja sen ympäristöön juurtuneessa kolmipäiväisessä festivalissa esiintyy laaja ja erinomainen kattaus kotimaisia ja kansainvälisiä artisteja",
@@ -26,7 +23,6 @@ let calendarData = [
       },
       {
             "date": "2023-06-10",
-            "opened": false,
             "img": "./Kuvat/helsinkipaiva.jpg",
             "link": "https://helsinkipaiva.fi/event/helsinki-paivan-puistojuhlat-lapinlahden-lahteella/",
             "desc": "Helsinki-päivänä 12.6. Lapinlahden vanhassa sairaalapuutarhassa vietetään jälleen puistojuhlaa, johon kutsumme kaikki helsinkiläiset juhlistamaan upeaa kaupunkiamme. Myös paljon muita tapahtumia.",
@@ -34,63 +30,54 @@ let calendarData = [
 
       }, {
             "date": "2023-06-17",
-            "opened": false,
             "img": "./Kuvat/Juhannusvalkeat.jpg",
             "link": "https://www.juhannusvalkeat.fi/",
             "desc": "TERVE­TULOA Juhannus­valkeille jälleen 21.6.2024!",
             "map": `<iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15873.754312534074!2d24.84738317910158!3d60.177102500000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920a111718eddb%3A0xe485e638e2ad0e4e!2sSeurasaari!5e0!3m2!1sen!2sfi!4v1714991457765!5m2!1sen!2sfi"></iframe>`
       }, {
             "date": "2023-06-24",
-            "opened": false,
             "img": "./Kuvat/tuska.jpg",
             "link": "https://tuska.fi",
             "desc": "Tuska Festivaali on pohjoismaiden suurimpia raskaanmusiikin tapahtumia, joka kokoaa yhteen raskaanmusiikin ystäviä kolmen päivän aikana.",
             "map": `<iframe id="map" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d954.3539308739814!2d24.969606894836954!3d60.18665014847988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1ssuvilahti!5e0!3m2!1sfi!2sfi!4v1714990762108!5m2!1sfi!2sfi"></iframe>`
       }, {
             "date": "2023-07-01",
-            "opened": false,
             "img": "./Kuvat/luomus.jpg",
             "link": "https://www.helsinki.fi/fi/luomus/yleisokohteet/luonnontieteellinen-museo",
             "desc": "Luonnontieteellisen museon ilmaispäivät ovat: 14.2.2024, 10.4.2024, 12.6.2024, 14.8.2024, 9.10.2024, 11.12.2024",
             "map": `<iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969.126126673927!2d24.92868587704801!3d60.17141197503306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920a330b5db7e1%3A0x70a9fb2eae7279c!2sFinnish%20Museum%20of%20Natural%20History!5e0!3m2!1sen!2sfi!4v1714992379149!5m2!1sen!2sfi"></iframe>`
       }, {
             "date": "2023-07-08",
-            "opened": false,
             "img": "./Kuvat/kiasma1.jpg",
             "link": "https://kiasma.fi/",
             "desc": "Kiasma on nykytaiteen museo keskellä Helsinkiä. Kiasma kerää ja tekee tunnetuksi oman aikamme taidetta.",
             "map": `<iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1984.552243900372!2d24.934289477168004!3d60.171591075033106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920a332ab06d07%3A0xa6c0f7efd55d1d15!2sMuseum%20of%20Contemporary%20Art%20Kiasma!5e0!3m2!1sen!2sfi!4v1714991973084!5m2!1sen!2sfi" </iframe>`
       }, {
             "date": "2023-07-15",
-            "opened": false,
             "img": "./Kuvat/helsinkisaaristoristeily1.jpg",
             "link": "https://www.myhelsinki.fi/fi/n%C3%A4e-ja-koe/aktiviteetit/risteilyj%C3%A4-helsingin-saaristossa",
             "desc": "Merellinen Helsinki aukeaa parhaiten saaristoristeilyllä. Maisemien lisäksi voit samalla nauttia vaikka lounaan, brunssin tai illallisen. Kaikki saaristoristeilyalukset lähtevät Kauppatorilta ja liikennöivät vain kesäkaudella (touko-syyskuu).",
             "map": `<iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5613.71198673517!2d24.945038839685072!3d60.16836866669434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920bcf117c4bb3%3A0xc5acf882c8111bd5!2sMarket%20Square!5e0!3m2!1sen!2sfi!4v1714990980112!5m2!1sen!2sfi" </iframe>`
       }, {
             "date": "2023-07-22",
-            "opened": false,
             "img": "./Kuvat/Vanhankaupunginkoskensuvanto1.jpg",
             "link": "https://www.vapaa-ajankalastaja.fi/kalastuspaikka/vanhankaupunginkosken-suvanto/",
             "desc": "Kalastusta Vanhankaupunginkosken suvannolla",
             "map": `<iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1981.944315328459!2d24.980555877170932!3d60.21475217505518!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920922abf79421%3A0x9f9da07a94d86a68!2sVanhankaupunginkosken%20suvanto!5e0!3m2!1sen!2sfi!4v1714990069288!5m2!1sen!2sfi"</iframe>`
       }, {
             "date": "2023-07-29",
-            "opened": false,
             "img": "./Kuvat/suomenlinna1.jpg",
             "link": "https://www.suomenlinna.fi/",
             "desc": "Unescon maailmanperintökohde Suomenlinna on kulttuuriaarre, jonka rakentaminen alkoi 1700-luvun puolivälissä Suomen kuuluessa Ruotsiin. Tervetuloa Suomenlinnaan!",
             "map": `<iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15888.646194521136!2d24.978678399999996!3d60.146285299999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920b8e716456fb%3A0xa00b553b99239b0!2sSveaborg%2C%20Helsinki!5e0!3m2!1sen!2sfi!4v1714989218965!5m2!1sen!2sfi" </iframe>`
       }, {
             "date": "2023-08-05",
-            "opened": false,
             "img": "./Kuvat/Uimastadion1.jpg",
             "link": "https://www.hel.fi/fi/kulttuuri-ja-vapaa-aika/ulkoilu-puistot-ja-luontokohteet/uimarannat/maauimalat/uimastadion",
             "desc": "Legendaarinen Uimastadion eli tuttavallisemmin Stadikka on Suomen vanhin maauimala. Maauimalasta löytyy iso allas, lasten allas, kahluuallas sekä hyppyallas hyppytorneineen. Vuosittaisia vetonauloja ovat erityisesti pellehyppykisat ja kuutamouinnit. ",
             "map": `<iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1983.5219162130693!2d24.92600369583213!3d60.188645177594815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920a2867cf14c7%3A0xbba80d4cd745fb74!2sHelsinki%20Swimming%20Stadium!5e0!3m2!1sen!2sfi!4v1714988902886!5m2!1sen!2sfi" </iframe>`
       }, {
             "date": "2023-08-12",
-            "opened": false,
             "img": "./Kuvat/Korkeasaari1.jpg",
             "link": "https://www.korkeasaari.fi/",
             "desc": "Korkeasaaressa pääset tutustumaan yli 150 eläinlajiin ja satoihin kasvilajeihin ympäri maailmaa. Vierailullasi tuet uhanalaisten eläinen suojelua.",
