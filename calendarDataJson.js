359d40b7361fa991766a17bf3bd2b3f31a91ca35
vsto99 <159899257+vsto99@users.noreply.github.com>
2024-05-06 12:56:05 +0300

    suomenlinna
    viikko 11 + kuva

--- calendarDataJson.js
+++ calendarDataJson.js
@@ -77,10 +77,10 @@ let calendarData = [
       }, {
             "date": "2024-07-29",
             "opened": false,
-            "img": "maailmakylassa.jpg",
-            "link": "",
-            "desc": "kaks",
-            "map": ``
+            "img": "./Kuvat/suomenlinna1.jpg",
+            "link": "https://www.suomenlinna.fi/",
+            "desc": "Unescon maailmanperintökohde Suomenlinna on kulttuuriaarre, jonka rakentaminen alkoi 1700-luvun puolivälissä Suomen kuuluessa Ruotsiin. Tervetuloa Suomenlinnaan!",
+            "map": `<iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15888.646194521136!2d24.978678399999996!3d60.146285299999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920b8e716456fb%3A0xa00b553b99239b0!2sSveaborg%2C%20Helsinki!5e0!3m2!1sen!2sfi!4v1714989218965!5m2!1sen!2sfi" </iframe>`
       }, {
             "date": "2023-08-05",
             "opened": false,
