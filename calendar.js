5c19ae6cfc42b8a39c0e10cb475bbc5c56b5868c
Valheri <159899638+Valheri@users.noreply.github.com>
2024-05-06 14:46:49 +0300

    Koodin siistimistä
    data.opened poisto. kuvien ja highlight muutos.

--- calendar.js
+++ calendar.js
@@ -1,36 +1,30 @@
-
-// Define your calendar data here
-
 const today = new Date();
 const todayString = today.toISOString().split('T')[0];
 const calendarContainer = document.getElementById('calendar');
-let i = 1;
 
-calendarData.forEach(data => {
-  const holder = document.createElement('div');
-  const door = document.createElement('div');
-  door.classList.add('door');
-  const para = document.createElement('p');
-  para.classList.add('teksti');
-  if (data.date <= todayString && !data.opened) {
-    door.classList.add('highlight');
-    door.innerHTML = `<img src="./Kuvat/doorclosed.jpg" width="100%" alt="Door">`;
-    door.addEventListener('click', () => {
-      data.opened = true;
-      door.classList.remove('highlight');
-      door.innerHTML = `<img src="./Kuvat/door.jpg" width="100%" alt="Door">`;
-      openModalPopup(data);
-    });
-  } else if (data.opened) {
-    door.innerHTML = `<img src="./Kuvat/door.jpg" width="100%" alt="Door">`;
-
-  } else {
-    door.innerHTML = `<img src="./Kuvat/doorclosed.jpg" width="100%" alt="Door">`;
-  }
-  para.innerHTML = `Viikko ${i}`;
-  holder.appendChild(door);
-  holder.appendChild(para);
-  calendarContainer.appendChild(holder);
-  i++;
+calendarData.forEach((data, index) => {
+    const holder = document.createElement('div');
+    const door = document.createElement('div');
+    door.classList.add('door');
+    const para = document.createElement('p');
+    para.classList.add('teksti');
+  
+    if (data.date <= todayString) {
+        door.classList.add('highlight');
+        door.innerHTML = `<img src="./Kuvat/door.jpg" width="100%" alt="Door">`;
+        door.addEventListener('click', () => {
+            openModalPopup(data);
+        });
+    } else {
+       door.classList.add('highlight2');
+       door.innerHTML = `<img src="./Kuvat/doorclosed.jpg" width="100%" alt="Door">`;
+    }
+  
+    para.innerHTML = `Viikko ${index + 1}`;
+    holder.appendChild(door);
+    holder.appendChild(para);
+    calendarContainer.appendChild(holder);
 });
+
+
 
