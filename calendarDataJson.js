421d136b9e1a7a83abca1e9ec851387cb4dd5e81
Valheri <159899638+Valheri@users.noreply.github.com>
2024-05-06 11:26:49 +0300

    testi kuvan lisäys
    maailmakylassa.jpg lisays

--- calendarDataJson.js
+++ calendarDataJson.js
@@ -2,7 +2,7 @@ let calendarData = [
       {
             "date": "2024-01-04",
             "opened": false,
-            "img": "maailmakylassa.jpg",
+            "img": "./Kuvat/maailmakylassa.jpg",
             "link": "http://example.com/1",
             "desc": "jotain",
             "map": `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d129708.19179114768!2d24.57343458998777!3d59.47158519202321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46929499df5616bf%3A0x400b36d18fc6270!2sTallinn%2C%20Estonia!5e0!3m2!1sen!2sfi!4v1714974541499!5m2!1sen!2sfi"></iframe>`
@@ -14,7 +14,7 @@ let calendarData = [
             "image_opened": "door.jpg",
             "link": "",
             "desc": "kaks",
-            "map": ``
+            "map": `` 
       },
       {
             "date": "2024-06-03",
